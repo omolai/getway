2c0bd4d17861b349059f2c64e8895c5ddf127585
ajun <3147678871@qq.com>
2026-08-11 10:31:25 +0800

    完善app_application

--- App/App_Application.c
+++ App/App_Application.c
@@ -2,6 +2,7 @@
 
 static DoubleBuffer *upBuffer;
 static DoubleBuffer *downBuffer;
+static bool app_isrunning = true;
 
 static void App_Application_MQTTReceiveHandle(int len, char *datas);
 static void App_Application_UpTaskFunc(void *args);
@@ -93,13 +94,33 @@ static void App_Application_Init(void)
     log_info("下行任务准备就绪");
 }
 
+void App_Application_Exit(int code)
+{
+    log_info("捕捉到信号:%d", code);
+    app_isrunning = false;
+}
+
 void App_Application_Run(void)
 {
 
     App_Application_Init();
+    // 捕获信号
+    signal(SIGINT, App_Application_Exit);
+    signal(SIGTERM, App_Application_Exit);
 
-    while (1)
-        ;
+    while (app_isrunning)
+    {
+        // 进入阻塞状态,一旦检测到信号之后开始运行
+        pause();
+    }
+
+    // 回收资源
+    log_info("准备回收资源");
+    Common_Buffer_Destory(upBuffer);
+    Common_Buffer_Destory(downBuffer);
+    Driver_Modbus_Destory();
+    Driver_MQTT_Deinit();
+    Common_Pool_Destory();
 }
 
 /**
@@ -133,6 +154,12 @@ static void App_Application_UpTaskFunc(void *args)
             log_info("从上行缓冲区获取到数据:%s", datas);
             // 通过MQTT发走
             Driver_MQTT_Send(PUSH_TOPIC, datas, size);
+            if (datas)
+            {
+
+                free(datas);
+                datas = NULL;
+            }
         }
     }
 }
@@ -230,6 +257,12 @@ static void App_Application_DownTaskFunc(void *args)
             }
 
             cJSON_Delete(root);
+            if (datas)
+            {
+
+                free(datas);
+                datas = NULL;
+            }
         }
     }
 }
